fe7932d6fa46a15117702ab0e997357afd23135b
Nandish Chauhan <202451040@iiitvadodara.ac.in>
2025-03-22 03:11:17 +0530

    update sol

--- src/main.py
+++ src/main.py
@@ -1,14 +1,63 @@
-def main(input_file_name = "testcase.txt", output_file_name = "output.txt"):
-    input_file = open(input_file_name, "r")
-    output_file = open(output_file_name, "w")
+import mmap
+from time import perf_counter
+# from statistics import mean
 
-    first_line = input_file.readline().strip()
-    first_line = first_line.split(";")
+dic:dict = {}
+output_arr:list = []
 
-    output_file.write(f"{first_line[0]}={first_line[1]}/{1}/{1}\n")
 
-    output_file.close()
-    input_file.close()
+def read(filename: str):
+    with open(filename, "rb") as file:
+        file_descriptor = file.fileno()
+        file_map = mmap.mmap(file_descriptor, 0, access=mmap.ACCESS_READ)
+        file_data = bytes(file_map)
+        file_map.close()
 
+    return file_data
+
+
+def processline(line: str) -> None:
+    global dic
+    arg = line.split(";")
+    val = float(arg[1])
+    if dic.get(arg[0]):
+        dic[arg[0]]['min'] = min(dic[arg[0]]['min'], val)
+        dic[arg[0]]['max'] = max(dic[arg[0]]['max'], val)
+        dic[arg[0]]['count']+=1
+        dic[arg[0]]['mean'] += (val - dic[arg[0]]['mean']) / dic[arg[0]]['count']
+    else:
+        dic[arg[0]] = {
+            'min': val,
+            'max': val,
+            'count': 1,
+            'mean': val
+        }
+    return
+
+
+def processline1(key, val) -> str:
+    global output_arr
+    output_arr.append(f"{key}={round(val['min'],1)}/{round(val['mean'],1)}/{round(val['max'],1)}\n")
+    return
+
+def sort_arr() -> None:
+    global dic
+    dic = dict(sorted(dic.items()))
+def main(input_file_name: str = "testcase.txt", output_file_name: str = "output.txt"):
+
+    a = perf_counter()
+    print("timer started")
+    input_dat = read(input_file_name).decode().strip()
+    for x in input_dat.splitlines():
+        processline(x)
+    sort_arr()
+    for x,y in dic.items():
+        processline1(x,y)
+        
+    with open(output_file_name, 'w') as f:
+        f.write("".join(output_arr))
+    return
 if __name__ == "__main__":
+    a = perf_counter()
     main()
+    print(perf_counter()-a)
